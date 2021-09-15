21c1d9126264e67a94edad312b3bbf2562f562ec
Sergio Araujo-Estrada <aesaae@bristol.ac.uk>
2021-09-15 13:39:55 +0100

    Modified usage_example.py by adding the folowing examples of usage:  - Extract list of Web of Science's research categories statistics,  - Extract list of Web of Science's research areas statistics,  - Plot Treemap of total (i.e., for all years) Web of Science's research categories and  - Plot Treemap of total (i.e., for all years) Web of Science's research areas.

--- usage_example.py
+++ usage_example.py
@@ -2,6 +2,7 @@ import read_wos_database
 #%%
 # Define path to datanase files
 path_to_file = 'C:\\Temp\\'
+#%%
 # Create 'read_wos_database' instance
 test=read_wos_database.read_wos_database()
 # Set path to file(s)
@@ -17,6 +18,10 @@ test.extract_pub_year_stats()
 test.extract_countries_list_stats()
 #%% Compute country publications per year
 test.compute_country_pub_per_year()
+#%% Extract list of Web of Science's research categories statistics
+test.extract_wos_categories_list_stats()
+#%% Extract list of Web of Science's research areas statistics
+test.extract_wos_research_areas_list_stats()
 
 #%% Plot publications per year distribution
 test.plot_pub_per_year()
@@ -27,3 +32,7 @@ test.plot_pub_per_country()
 #%% Plot publications per country per year distribution
 test.plot_country_pub_per_year()
 #test.plot_country_pub_per_year(norm=True)
+#%% Plot Treemap of total (i.e., for all years) Web of Science's research categories
+test.plot_treemap_wos_categories_total(show_labels=False)
+#%% Plot Treemap of total (i.e., for all years) Web of Science's research areas
+test.plot_treemap_wos_research_areas_total()
